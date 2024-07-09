888ad0ffb8ff28354380c16df80a70cce1991d33
uday <udaysah0003@gmail.com>
2024-07-09 19:59:35 +0530

    css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Grid, useMediaQuery, useTheme } from "@mui/material";
 import Header from "../components/Header";
 import ChatList from "../components/ChatList";
 import ChatWindow from "../components/ChatWindow";
 import MobileChat from "../components/MobileChat";
 import { useTheme as useCustomTheme } from "../context/ThemeContext";
 
-import darkBg from "../assets/darkbg.webp"
-import lightBg from "../assets/lightbg.jpg"
-
-
-
+import darkBg from "../assets/darkbg.webp";
+import lightBg from "../assets/lightbg.jpg";
 
 const Home: React.FC = () => {
   const theme = useTheme();
   const { theme: customTheme } = useCustomTheme();
 
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const background = customTheme === "dark" ? darkBg : lightBg;
 
   return (
@@ -25,26 +22,24 @@ const Home: React.FC = () => {
       {isMobile ? (
         <MobileChat />
       ) : (
-        <div
-          style={{
-            height: "100vh",
-            width: "100vw",
-            backgroundImage: `url(${background})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <ChatList />
-          <Box sx={{
-            flex: 3, overflowY: "auto", backgroundImage: background, backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }} >
-            <ChatWindow />
-          </Box>
-        </div>
+        <Grid container sx={{ height: "calc(100% - 56px)" }}>
+          <Grid item xs={12} md={3}>
+            <ChatList />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Box
+              sx={{
+                height: "100%",
+                backgroundImage: `url(${background})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            >
+              <ChatWindow />
+            </Box>
+          </Grid>
+        </Grid>
       )}
     </Box>
   );
