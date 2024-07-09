d8aa14aaf77e402aeb0f52636bca2223f7453643
uday <udaysah0003@gmail.com>
2024-07-09 20:14:55 +0530

    css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaSearch } from 'react-icons/fa';
 import './Header.css';
-import { Menu, MenuItem } from '@mui/material';
+import { Box, Grid, Menu, MenuItem } from '@mui/material';
 import MessageIcon from '@mui/icons-material/Message';
 import Switch from '@mui/material/Switch';
 import Slider from '@mui/material/Slider';
@@ -22,24 +22,20 @@ import { useTheme } from '../context/ThemeContext';
 import ChatWindowHead from './ChatWindowHead';
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
 const Header: React.FC = () => {
   const [showTabs, setShowTabs] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
   const { theme, toggleTheme } = useTheme();
+
   const handleSearchClick = () => {
     setShowTabs(true);
   };
 
-
-
   const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     setAnchorEl(event.currentTarget);
-  };
-
-  const themeChange = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-
   };
 
   const handleClose = () => {
@@ -47,17 +43,17 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className='headCOntainer'>
-      <div className="header">
-        <div className="menu-button" style={{ fontSize: 16 }} onClick={handleClick}>
-          <FaBars />
-        </div>
-        <div>
+    <Box sx={{ height: "64px", display: "flex", alignItems: "center", backgroundColor: theme === 'dark' ? '#333' : '#fff', borderBottom: `1px solid ${theme === 'dark' ? '#444' : '#ddd'}` }}>
+      <Grid container alignItems="center">
+        <Grid item xs={12} md={3} sx={{ display: 'flex', alignItems: 'center' }}>
+          <div className="menu-button" style={{ fontSize: 16 }} onClick={handleClick}>
+            <FaBars />
+          </div>
           <Menu
             id="basic-menu"
             anchorEl={anchorEl}
             open={open}
-            // onClose={handleClose}
+            onClose={handleClose}
             MenuListProps={{
               'aria-labelledby': 'basic-button',
             }}
@@ -67,32 +63,30 @@ const Header: React.FC = () => {
             <MenuItem onClick={handleClose} className='MenuItem'><ContactMailOutlined className="icon" />Contacts</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><Timelapse className="icon" />My Stories</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><Settings className="icon" />Settings</MenuItem>
-            <MenuItem onClick={handleClose} className='MenuItem'><NightlifeSharp className="icon" />Night Mode      <Switch {...label} checked={theme === "dark" ? true : false} size="small" onClick={toggleTheme} /></MenuItem>
-            <MenuItem onClick={handleClose} className='MenuItem'><Animation className="icon" />Animation    <Slider style={{ width: 40 }} defaultValue={10} aria-label="Default" valueLabelDisplay="auto" /></MenuItem>
+            <MenuItem onClick={handleClose} className='MenuItem'><NightlifeSharp className="icon" />Night Mode <Switch {...label} checked={theme === "dark"} size="small" onClick={toggleTheme} /></MenuItem>
+            <MenuItem onClick={handleClose} className='MenuItem'><Animation className="icon" />Animation <Slider style={{ width: 40 }} defaultValue={10} aria-label="Default" valueLabelDisplay="auto" /></MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><FeaturedPlayList className="icon" />Telegram Features</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><BugReport className="icon" />Report a Bug</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><KebabDiningRounded className="icon" />Switch to K Version</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><AppBlockingOutlined className="icon" />Install App</MenuItem>
             <MenuItem onClick={handleClose} className='MenuItem'><Logout className="icon" />Logout</MenuItem>
           </Menu>
-        </div>
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder={`Search`}
-            onClick={handleSearchClick}
-          />
-          <FaSearch className="search-icon" />
+          <div className="search-bar" style={{ marginLeft: '16px', flex: 1 }}>
+            <input
+              type="text"
+              placeholder={`Search`}
+              onClick={handleSearchClick}
+            />
+            <FaSearch className="search-icon" />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={9} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <ChatWindowHead />
+        </Grid>
+      </Grid>
 
-        </div>
-      </div>
-
-      {showTabs && (
-        <TabsComponent />
-      )}
-      <ChatWindowHead />
-
-    </div>
+      {showTabs && <TabsComponent />}
+    </Box>
   );
 };
 
